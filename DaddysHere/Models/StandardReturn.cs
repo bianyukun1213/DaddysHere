0b63e644f4f978e1534dc61b4dd06a3e28c4471f
边宇琨 <bianyukun1213@outlook.com>
2022-07-10 12:41:14 +0800

    Update StandardReturn.cs

--- DaddysHere/Models/StandardReturn.cs
+++ DaddysHere/Models/StandardReturn.cs
@@ -21,43 +21,6 @@ namespace DaddysHere.Models
         public object? Result { get; set; }
         public StandardReturn(IStringLocalizer<SharedResource> localizer, ErrorType errorType = ErrorType.Succeeded, object? result = null)
         {
-            //switch (errorType)
-            //{
-            //    //1：系统，2：数据库，3：第三方。
-            //    case ErrorType.Succeeded:
-            //        Code = 0;
-            //        Msg = localizer["succeeded"];
-            //        Result = result;
-            //        break;
-            //    case ErrorType.UnknownError:
-            //        Code = -1;
-            //        Msg = localizer["unknownError"];
-            //        break;
-            //    case ErrorType.DataNotFound:
-            //        Code = 20001;
-            //        Msg = localizer["dataNotFound"];
-            //        break;
-            //    case ErrorType.WrongData:
-            //        Code = 20002;
-            //        Msg = localizer["wrongData"];
-            //        break;
-            //    case ErrorType.PermissionDenied:
-            //        Code = 20003;
-            //        Msg = localizer["permissionDenied"];
-            //        break;
-            //    case ErrorType.:
-            //        Code = 20004;
-            //        Msg = localizer["repeatedSubmissionNotAllowed"];
-            //        break;
-            //    case 20005:
-            //        Code = 20005;
-            //        Msg = localizer["reachLimitValue"];
-            //        break;
-            //    case 30001:
-            //        Code = 30001;
-            //        Msg = localizer["thirdPartyServiceError"];
-            //        break;
-            //}
             Code = (int)errorType;
             Msg = localizer[errorType.ToString()];
             Result = result;
